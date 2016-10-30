c0698e091aa7f8157244e303f58e9a4b3a0b2d88
if1live <libsora25@gmail.com>
2016-10-31 00:53:36 +0900

    hitomi 같아보이면 찾아서 업로드

--- hitomi_detector.go
+++ hitomi_detector.go
@@ -1,74 +1,117 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"regexp"
 	"strconv"
 	"strings"
 
-	"path"
-
 	"github.com/ChimeraCoder/anaconda"
+	dropy "github.com/tj/go-dropy"
 )
 
 const (
-	NotFound = -1
+	notFound           = -1
+	hitomiSavePath     = "/hitomi-temp"
+	haruPath           = "../haru"
+	haruExecutableName = "haru"
 )
 
 type HitomiDetector struct {
 	config *Config
+	client *dropy.Client
 }
 
 func NewHitomiDetector(config *Config) *HitomiDetector {
+	client := config.CreateDropboxClient()
 	detector := &HitomiDetector{
 		config,
+		client,
 	}
 	return detector
 }
 
 func (d *HitomiDetector) OnTweet(tweet *anaconda.Tweet) {
-	d.ProcessText(tweet.Text)
+	d.ProcessText(tweet.Text, tweet.IdStr)
 }
 func (d *HitomiDetector) OnFavorite(tweet *anaconda.EventTweet) {
 	if tweet.Source.ScreenName != d.config.DataSourceScreenName {
 		return
 	}
-	d.ProcessText(tweet.TargetObject.Text)
+	d.ProcessText(tweet.TargetObject.Text, tweet.TargetObject.IdStr)
 }
 func (d *HitomiDetector) OnUnfavorite(tweet *anaconda.EventTweet) {
 }
 
-func (d *HitomiDetector) ProcessText(text string) {
+func (d *HitomiDetector) ProcessText(text string, tweetId string) {
 	code := d.FindReaderNumber(text)
-	if code == NotFound {
+	if code == notFound {
 		return
 	}
 
-	currDir := GetExecutablePath()
-	haruPath := path.Join(currDir, "..", "haru")
-	haruExeName := "haru"
-	haruFilePath := path.Join(haruPath, haruExeName)
-	idParam := fmt.Sprintf("-id=%d", code)
-	out, err := exec.Command(haruFilePath, idParam, "-service=hitomi", "-cmd=download").CombinedOutput()
+	log.Printf("HitomiDetector Found Code %d", code)
 
-	elems := []string{}
+	currDir := GetExecutablePath()
+	cmd := path.Join(currDir, haruPath, haruExecutableName)
+	args := []string{
+		fmt.Sprintf("-id=%d", code),
+		"-service=hitomi",
+		"-cmd=download",
+	}
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+
+	// dump stdout/stderr
+	stderrs := []string{}
 	if err != nil {
 		if _, ok := err.(*exec.Error); ok {
-			elems = append(elems, err.Error())
+			stderrs = append(stderrs, err.Error())
 		}
 	}
-	elems = strings.Split(string(out[:]), "\n")
+	stdouts := strings.Split(string(out[:]), "\n")
 
-	for _, line := range elems {
-		fmt.Println(line)
+	for _, line := range stderrs {
+		log.Println(line)
+	}
+	for _, line := range stdouts {
+		log.Println(line)
 	}
 
+	// 공백이 아닌 가장 마지막 출력 찾기
+	// 거기에 파일명이 있을거다
+	lastStdout := ""
+	for i := len(stdouts) - 1; i >= 0; i-- {
+		if len(stdouts[i]) > 0 {
+			lastStdout = stdouts[i]
+			break
+		}
+	}
+
+	zipFilename := ""
+	re := regexp.MustCompile(` (/.*\.zip)`)
+	for _, m := range re.FindAllStringSubmatch(lastStdout, -1) {
+		zipFilename = m[1]
+	}
+	log.Printf("HitomiDetector Haru Complete %s, %s", zipFilename, tweetId)
+
+	// upload
+	baseZipFileName := filepath.Base(zipFilename)
+	uploadFilePath := path.Join(hitomiSavePath, baseZipFileName)
+	file, _ := os.Open(zipFilename)
+	r := bufio.NewReader(file)
+	d.client.Upload(uploadFilePath, r)
+
+	log.Printf("HitomiDetector Complete %s", tweetId)
 }
 
 func (d *HitomiDetector) FindReaderNumber(text string) int {
 	if len(text) == 0 {
-		return NotFound
+		return notFound
 	}
 
 	if len(text) == 6 {
@@ -77,7 +120,7 @@ func (d *HitomiDetector) FindReaderNumber(text string) int {
 			val, _ := strconv.Atoi(text)
 			return val
 		}
-		return NotFound
+		return notFound
 	}
 
 	// 7글자 이상이면 7자리 숫자일떄의 예외처리가 필요
@@ -86,5 +129,5 @@ func (d *HitomiDetector) FindReaderNumber(text string) int {
 		val, _ := strconv.Atoi(m[1])
 		return val
 	}
-	return NotFound
+	return notFound
 }
